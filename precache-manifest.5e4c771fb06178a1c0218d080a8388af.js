97874e8f9da066c9395f11051d3eaf08f665457f
fonmagnus <arnold777armageddon@gmail.com>
2020-07-03 16:36:18 +0700

    Updates

---
+++ precache-manifest.5e4c771fb06178a1c0218d080a8388af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee3921330dfa2d247dc1d9fc8c0c4d2",
+    "url": "/react-recipes-generator/index.html"
+  },
+  {
+    "revision": "1e521d848278f87e7e1c",
+    "url": "/react-recipes-generator/static/css/main.c549ce9c.chunk.css"
+  },
+  {
+    "revision": "b7771c6b7375826e66ee",
+    "url": "/react-recipes-generator/static/js/2.953e3c0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipes-generator/static/js/2.953e3c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e521d848278f87e7e1c",
+    "url": "/react-recipes-generator/static/js/main.7618f286.chunk.js"
+  },
+  {
+    "revision": "4bf099f68e52013bb23e",
+    "url": "/react-recipes-generator/static/js/runtime-main.f8dda438.js"
+  }
+]);
